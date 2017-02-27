4f9258b318873f879e89fa57f53788f985c23048
xm94630 <xm94630@126.com>
2017-02-27 14:12:59 +0800

    继续链式

--- src/js/common/chapter13.js
+++ src/js/common/chapter13.js
@@ -25,7 +25,7 @@ var bee = (function(bee){
 			},
 			getName:function(){
 				return this.name;
-			},
+			}
 		}
 		var n = obj.setName('兰陵王').getName();
 		l(n);
@@ -51,7 +51,7 @@ var bee = (function(bee){
 				},
 				getName:function(){
 					return name;
-				},
+				}
 			}
 		}
 
@@ -79,7 +79,7 @@ var bee = (function(bee){
 				},
 				getName:function(){
 					return name;
-				},
+				}
 			}
 		}
 
@@ -90,6 +90,80 @@ var bee = (function(bee){
 		var n3 = kingCreate('兰陵王').setName('甄姬').getName();
 		l(n3);
 	}
+
+
+	//研究案例5: 现在我们把 kingCreate 挂在到一个对象上
+	bee.caseM5 = function(){
+
+		var king = {
+			kingCreate : function(name){
+				var name = name||'程咬金';
+				return {
+					setName:function(n){
+						name = n;
+						return this;
+					},
+					getName:function(){
+						return name;
+					}
+				}
+			}
+		}
+
+		//这种情况下，使用还是差不多的，结果也一样
+		var n1 = king.kingCreate().getName();
+		l(n1);
+		var n2 = king.kingCreate('兰陵王').getName();
+		l(n2);
+		var n3 = king.kingCreate('兰陵王').setName('甄姬').getName();
+		l(n3);
+
+		//需要注意的是：链式调用在 kingCreate 之后才开始（才使用return this）,在getName函数终止（没有在return this了）。
+		//对于 king 本身并没有可以链式的（至少我这里没有这样子做）
+		//所以，getName 方法在king上还是没有的哦~ 使用下面的会报错：
+		
+		//var n4 = king.getName();
+		//l(n4);
+
+		//我这里的模式，其实是等价于 underscore 中的：
+		//_.chain({}).cort().value();
+		//_就好比我的king，chain是链式的开始，就好比我的kingCreate
+		//value是链式的终止，就好比我的getName。
+
+		//不同之处是，我的链式只是采用了比较简单的字符串的处理。
+		//而他则是对数组和对象的高级操作！
+	}
+
+	//研究案例6: 在链式中 打入！（围棋的术语）
+	//高阶函数大展身手！
+	bee.caseM6 = function(){
+
+		var king = {
+			kingCreate : function(name){
+				var name = name||'程咬金';
+				return {
+					setName:function(n){
+						name = n;
+						return this;
+					},
+					getName:function(){
+						return name;
+					},
+					tap:function(fun){
+						fun(name);
+						return this;
+					}
+				}
+			}
+		}
+
+		//通过tap就可以提取链中的数据
+		king.kingCreate('兰陵王').setName('孙悟空').tap(function(n){
+			l('我就是'+n);
+		}).setName('甄姬').getName();
+	}
+
+
 
 
 

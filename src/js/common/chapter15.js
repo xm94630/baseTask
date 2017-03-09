bd3d851e3c0c98398478fc880d2e00de31c50f37
xm94630 <xm94630@126.com>
2017-03-09 21:21:39 +0800

    简化id生成器！

--- src/js/common/chapter15.js
+++ src/js/common/chapter15.js
@@ -828,6 +828,166 @@ var bee = (function(bee){
 
 
 
+function IdFactory() {
+	var i = 0;
+	return {getId:function(){return i++;}}
+}
+var idCreator = IdFactory();
+
+function Subscriber(fn, options, context) {
+	this.id = idCreator.getId();
+	this.fn = fn;
+	this.options = options;
+	this.context = context;
+	this.topic = null;
+}
+
+function Topic(namespace) {
+	this.namespace = namespace || "";
+	this._callbacks = [];
+	this._topics = [];
+	this.stopped = false;
+}
+
+Topic.prototype = {
+
+	AddSubscriber: function(fn, options, context) {
+		var callback = new Subscriber(fn, options, context);
+		this._callbacks.push(callback);
+		callback.topic = this;
+		return callback;
+	},
+
+	StopPropagation: function() {
+		this.stopped = true;
+	},
+
+	GetSubscriber: function(identifier) {
+		for (var x = 0, y = this._callbacks.length; x < y; x++) {
+			if (this._callbacks[x].id == identifier || this._callbacks[x].fn == identifier) {
+				return this._callbacks[x];
+			}
+		}
+		for (var z in this._topics) {
+			if (this._topics.hasOwnProperty(z)) {
+				var sub = this._topics[z].GetSubscriber(identifier);
+				if (sub !== undefined) {
+					return sub;
+				}
+			}
+		}
+	},
+
+	AddTopic: function(topic) {
+		this._topics[topic] = new Topic((this.namespace ? this.namespace + ":" : "") + topic);
+	},
+
+	HasTopic: function(topic) {
+		return this._topics.hasOwnProperty(topic);
+	},
+
+	ReturnTopic: function(topic) {
+		return this._topics[topic];
+	},
+
+	RemoveSubscriber: function(identifier) {
+		if (!identifier) {
+			this._callbacks = [];
+			for (var z in this._topics) {
+				if (this._topics.hasOwnProperty(z)) {
+					this._topics[z].RemoveSubscriber(identifier);
+				}
+			}
+		}
+		for (var y = 0, x = this._callbacks.length; y < x; y++) {
+			if (this._callbacks[y].fn == identifier || this._callbacks[y].id == identifier) {
+				this._callbacks[y].topic = null;
+				this._callbacks.splice(y, 1);
+				x--;
+				y--;
+			}
+		}
+	},
+
+	Publish: function(data) {
+		for (var y = 0, x = this._callbacks.length; y < x; y++) {
+			var callback = this._callbacks[y],l;
+			callback.fn.apply(callback.context, data);
+			l = this._callbacks.length;
+			if (l < x) {
+				y--;
+				x = l;
+			}
+		}
+		for (var x in this._topics) {
+			if (!this.stopped) {
+				if (this._topics.hasOwnProperty(x)) {
+					this._topics[x].Publish(data);
+				}
+			}
+		}
+		this.stopped = false;
+	}
+};
+
+
+function Mediator() {
+	this._topics = new Topic("");
+};
+
+Mediator.prototype = {
+	GetTopic: function(namespace) {
+		var topic = this._topics,
+			namespaceHierarchy = namespace.split(":");
+		if (namespace === "") {
+			return topic;
+		}
+		if (namespaceHierarchy.length > 0) {
+			for (var i = 0, j = namespaceHierarchy.length; i < j; i++) {
+				if (!topic.HasTopic(namespaceHierarchy[i])) {
+					topic.AddTopic(namespaceHierarchy[i]);
+				}
+				topic = topic.ReturnTopic(namespaceHierarchy[i]);
+			}
+		}
+		return topic;
+	},
+
+	//订阅
+	Subscribe: function(topicName, fn, options, context) {
+		var options = options || {},
+			context = context || {},
+			topic = this.GetTopic(topicName),
+			sub = topic.AddSubscriber(fn, options, context);
+		return sub;
+	},
+
+	//获取订阅者
+	GetSubscriber: function(identifier, topic) {
+		return this.GetTopic(topic || "").GetSubscriber(identifier);
+	},
+
+	//移除订阅者
+	Remove: function(topicName, identifier) {
+		this.GetTopic(topicName).RemoveSubscriber(identifier);
+	},
+
+	//发布
+	Publish: function(topicName) {
+		var args = Array.prototype.slice.call(arguments, 1),
+			topic = this.GetTopic(topicName);
+		args.push(topic);
+		this.GetTopic(topicName).Publish(args);
+	}
+};
+
+
+var m = new Mediator()
+m.Subscribe("inbox:messages:new",function(){l(111)});
+m.Subscribe("inbox:messages:new",function(){l(222)});
+m.Publish( "inbox");
+
+
 
 
 

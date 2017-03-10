88e42c5a0e4f51d9afeabc4e98b5757b76e34b05
xm94630 <xm94630@126.com>
2017-03-10 23:31:19 +0800

    添加案例 中介者模式，其实是发布订阅的加强版

--- src/js/common/chapter15.js
+++ src/js/common/chapter15.js
@@ -336,6 +336,34 @@ var bee = (function(bee){
 	}
 
 
+	//研究案例5: 中介者模式
+	//其实在我看来还是发布订阅模式的加强版
+	//之前是有了channel层的抽象，如果我们对 channel 抽象做的更加的完美，比如：
+	// "king:red:llw" 这样子具有层级关系的（命名空间）来控制订阅者，就更加完没了。
+	bee.caseO5 = function(){
+		//在 Mediator.js 中就实现了这样子的模式。这里不具体实现。主要说说这个主题对象（相当于channel，但是更加抽象）：
+
+		//该模式中，比较核心部分，也是逻辑比较复杂的地方，就是“主题”对象。
+		//它是这么定义的（在订阅的时候）：
+
+		//首先，一上来就是一个主题对象。
+		//第一个层级的主题对象（根），默认命名空间为空。
+		//如果topicName参数为空字符串，该主题对象中就不再嵌套子集主题了。（换句话说，就是属性_topics是空数组）
+		//如果topicName参数为字符串“king”，该主题对象的属性_topics，就有一个“king”字段的属性，其值是一个二级的主题对象。
+			//然后，现在就是在子集主题对象中了。
+			//第二个层级的主题对象，默认的命名空间为“king”
+			//如果，topicName参数为字符串“king:blue”，那么该二级主题对象的属性_topics，就有一个“blue”字段的属性，其值是一个第三级的主题对象。
+				//继续，现在进入该主题对象
+				//第三层级的主题对象，默认的命名空间为“king:blue”
+				//如果，topicName参数为字符串“king:blue:llw”，那么该三级主题对象的属性_topics，就有一个“llw”字段的属性，其值是一个第四级的主题对象。
+					//继续，现在进入该主题对象
+					//第四层级的主题对象，默认的命名空间为“king:blue:llw”
+					//如果，topicName参数为字符串“king:blue:llw”到此结束，那么该三级主题对象的属性_topics，再也没有子级主题对象了~~
+
+		//这是个递归的操作。相对复杂点~
+	}
+
+
 	//研究案例6: 【BOSS】 网络模式
 	//即是信息的发布者，也是订阅者
 	//这种模式我在项目中的需求中悟出来的，就是有一个互斥的实例。
@@ -822,167 +850,6 @@ var bee = (function(bee){
 
 
 
-
-
-
-
-
-
-function IdFactory() {
-	var i = 0;
-	return {getId:function(){return i++;}}
-}
-var idCreator = IdFactory();
-
-function Subscriber(fn, options, context) {
-	this.id = idCreator.getId();
-	this.fn = fn;
-	this.options = options;
-	this.context = context;
-	this.topic = null;
-}
-
-function Topic(namespace) {
-	this.namespace = namespace || "";
-	this._callbacks = [];
-	this._topics = [];
-	this.stopped = false;
-}
-
-Topic.prototype = {
-
-	AddSubscriber: function(fn, options, context) {
-		var callback = new Subscriber(fn, options, context);
-		this._callbacks.push(callback);
-		callback.topic = this;
-		return callback;
-	},
-
-	StopPropagation: function() {
-		this.stopped = true;
-	},
-
-	GetSubscriber: function(identifier) {
-		for (var x = 0, y = this._callbacks.length; x < y; x++) {
-			if (this._callbacks[x].id == identifier || this._callbacks[x].fn == identifier) {
-				return this._callbacks[x];
-			}
-		}
-		for (var z in this._topics) {
-			if (this._topics.hasOwnProperty(z)) {
-				var sub = this._topics[z].GetSubscriber(identifier);
-				if (sub !== undefined) {
-					return sub;
-				}
-			}
-		}
-	},
-
-	AddTopic: function(topic) {
-		this._topics[topic] = new Topic((this.namespace ? this.namespace + ":" : "") + topic);
-	},
-
-	HasTopic: function(topic) {
-		return this._topics.hasOwnProperty(topic);
-	},
-
-	ReturnTopic: function(topic) {
-		return this._topics[topic];
-	},
-
-	RemoveSubscriber: function(identifier) {
-		if (!identifier) {
-			this._callbacks = [];
-			for (var z in this._topics) {
-				if (this._topics.hasOwnProperty(z)) {
-					this._topics[z].RemoveSubscriber(identifier);
-				}
-			}
-		}
-		for (var y = 0, x = this._callbacks.length; y < x; y++) {
-			if (this._callbacks[y].fn == identifier || this._callbacks[y].id == identifier) {
-				this._callbacks[y].topic = null;
-				this._callbacks.splice(y, 1);
-				x--;
-				y--;
-			}
-		}
-	},
-
-	Publish: function(data) {
-		for (var y = 0, x = this._callbacks.length; y < x; y++) {
-			var callback = this._callbacks[y],l;
-			callback.fn.apply(callback.context, data);
-			l = this._callbacks.length;
-			if (l < x) {
-				y--;
-				x = l;
-			}
-		}
-		for (var x in this._topics) {
-			if (!this.stopped) {
-				if (this._topics.hasOwnProperty(x)) {
-					this._topics[x].Publish(data);
-				}
-			}
-		}
-		this.stopped = false;
-	}
-};
-
-
-function Mediator() {
-	this._topics = new Topic("");
-};
-
-Mediator.prototype = {
-	//订阅
-	Subscribe: function(topicName, fn, options, context) {
-		var options = options || {},
-			context = context || {},
-			topic = this.GetTopic(topicName),
-			sub = topic.AddSubscriber(fn, options, context);
-		return sub;
-	},
-	//发布
-	Publish: function(topicName) {
-		var args = Array.prototype.slice.call(arguments, 1),
-			topic = this.GetTopic(topicName);
-		args.push(topic);
-		this.GetTopic(topicName).Publish(args);
-	},
-	//移除订阅者
-	Remove: function(topicName, identifier) {
-		this.GetTopic(topicName).RemoveSubscriber(identifier);
-	},
-	//获取订阅者
-	GetSubscriber: function(identifier, topic) {
-		return this.GetTopic(topic || "").GetSubscriber(identifier);
-	},
-	//获取主题
-	GetTopic: function(namespace) {
-		var topic = this._topics,
-			namespaceHierarchy = namespace.split(":");
-		if (namespace === "") {
-			return topic;
-		}
-		if (namespaceHierarchy.length > 0) {
-			for (var i = 0, j = namespaceHierarchy.length; i < j; i++) {
-				if (!topic.HasTopic(namespaceHierarchy[i])) {
-					topic.AddTopic(namespaceHierarchy[i]);
-				}
-				topic = topic.ReturnTopic(namespaceHierarchy[i]);
-			}
-		}
-		return topic;
-	}
-};
-
-
-var m = new Mediator();
-m.Subscribe("inbox:messages:new",function(){l(111)});
-m.Subscribe("inbox:messages:new",function(){l(222)});
-m.Publish( "inbox");
 
 
 
